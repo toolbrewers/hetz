476ced95a581ee396d70b032089426a674fedf9b
Kevin Suñer <ksuner@pm.me>
2024-11-23 18:54:51 +0100

    rename public folder route, add route to serve app/assets folder

--- config/routes.go
+++ config/routes.go
@@ -9,7 +9,8 @@ import (
 func LoadRoutes(e *echo.Echo, controller *controllers.Controller) {
 	// Registers routes to serve static files such as images, javascript, html,
 	// css, pdf, fonts and so on
-	e.Static("/", "public")
+	e.Static("/public", "public")
+	e.Static("/assets", "app/assets")
 
 	// Reveal health status on /up that returns 200 if the app boots without errors,
 	// otherwise returns 500. Can be used by load balancers and uptime monitors to
