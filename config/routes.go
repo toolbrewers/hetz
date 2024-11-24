2c31dd0ca91abd5a48e65bd0fe2b4aa3ca15ca09
Kevin Suñer <ksuner@pm.me>
2024-11-24 19:45:17 +0100

    rename public route

--- config/routes.go
+++ config/routes.go
@@ -9,7 +9,7 @@ import (
 func LoadRoutes(e *echo.Echo, controller *controllers.Controller) {
 	// Registers routes to serve static files such as images, javascript, html,
 	// css, pdf, fonts and so on
-	e.Static("/public", "public")
+	e.Static("/", "public")
 	e.Static("/assets", "app/assets")
 
 	// Reveal health status on /up that returns 200 if the app boots without errors,
