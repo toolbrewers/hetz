c61e8eefaa9f903c008db2bc3d07d1ef8ef19994
Dejan Gegic <dejangegic100@gmail.com>
2024-11-28 22:35:19 +0100

    feat: Removed hetzner token UNIQUE constraint.

--- db/migrations/0001_init_schema.up.sql
+++ db/migrations/0001_init_schema.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     username        TEXT UNIQUE NOT NULL,
     email           TEXT UNIQUE NOT NULL,
     password        TEXT NOT NULL,
-    hetzner_token   TEXT UNIQUE NOT NULL,
+    hetzner_token   TEXT NOT NULL,
     created_at      DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at      DATETIME,
     deleted_at      DATETIME
