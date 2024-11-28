d0a8fe9cf05adc47e1cd91ffb4ab2a542555ac0d
Dejan Gegic <dejangegic100@gmail.com>
2024-11-28 20:08:04 +0100

    fix(db): Updated 0001 schema to be compatible with sqlite

--- db/migrations/0001_init_schema.up.sql
+++ db/migrations/0001_init_schema.up.sql
@@ -1,12 +1,11 @@
 -- Create users table schema
-CREATE TABLE users (
-    id              BIGSERIAL       NOT NULL,
-    username        VARCHAR(32)     UNIQUE NOT NULL,
-    email           VARCHAR(255)    UNIQUE NOT NULL,
-    password        TEXT            NOT NULL,
-    hetzner_token   TEXT            UNIQUE NOT NULL,
-    created_at      TIMESTAMP       NOT NULL DEFAULT NOW(),
-    updated_at      TIMESTAMP       NULL,
-    deleted_at      TIMESTAMP       NULL,
-    PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS users (
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    username        TEXT UNIQUE NOT NULL,
+    email           TEXT UNIQUE NOT NULL,
+    password        TEXT NOT NULL,
+    hetzner_token   TEXT UNIQUE NOT NULL,
+    created_at      DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at      DATETIME,
+    deleted_at      DATETIME
 );
