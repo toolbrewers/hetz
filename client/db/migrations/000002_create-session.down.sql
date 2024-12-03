cb27be3b16ad4f4458dccaa4630575ea068a632e
Dejan Gegic <dejangegic100@gmail.com>
2024-12-03 16:35:45 +0100

    feat(migrations): added roles table

--- client/db/migrations/000002_create-session.down.sql
+++ client/db/migrations/000002_create-session.down.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS roles;
--- client/db/migrations/000002_create-session.up.sql
+++ client/db/migrations/000002_create-session.up.sql
@@ -1,10 +1,19 @@
+CREATE TABLE IF NOT EXISTS roles (
+    name            TEXT        NOT NULL PRIMARY KEY,
+    created_at      DATETIME    DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at      DATETIME,
+    deleted_at      DATETIME
+);
+
 CREATE TABLE IF NOT EXISTS sessions (
     id              INTEGER     PRIMARY KEY AUTOINCREMENT,
     user_id         INTEGER     NOT NULL,
+    role_id         TEXT        NOT NULL,
     token           TEXT        NOT NULL,
     expires_at      DATETIME    NOT NULL,
     created_at      DATETIME    DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at      DATETIME,
     deleted_at      DATETIME,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (role_id) REFERENCES roles(name)
 );
