4887186e648365afe5eefef9d4f39ef902b5b5ae
Dejan Gegic <dejangegic100@gmail.com>
2024-12-04 11:49:52 +0100

    feat(migrate): Indexed token in sessions

--- client/db/migrations/000002_create-session.up.sql
+++ client/db/migrations/000002_create-session.up.sql
@@ -9,11 +9,12 @@ CREATE TABLE IF NOT EXISTS sessions (
     id              INTEGER     PRIMARY KEY AUTOINCREMENT,
     user_id         INTEGER     NOT NULL,
     role_id         TEXT        NOT NULL,
-    token           TEXT        NOT NULL,
+    token           TEXT        NOT NULL, -- Varchar(64) is all that is needed
     expires_at      DATETIME    NOT NULL,
     created_at      DATETIME    DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at      DATETIME,
     deleted_at      DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (role_id) REFERENCES roles(name)
+    FOREIGN KEY (role_id) REFERENCES roles(name),
+    INDEX (token)
 );
