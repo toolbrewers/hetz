ae0c29110f2d5a317c55dc9c982f8427a425f377
Dejan Gegic <dejangegic100@gmail.com>
2024-12-06 17:47:38 +0100

    feat(models): Model updates

--- client/internal/models/session.go
+++ client/internal/models/session.go
@@ -5,10 +5,16 @@ import "time"
 type Session struct {
 	ID        int       `db:"id"`
 	UserID    int       `db:"user_id"`
-	RoleID    string    `db:"role_id"`
 	Token     string    `db:"token"`
 	ExpiresAt time.Time `db:"expires_at"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
+	DeletedAt time.Time `db:"deleted_at"`
+}
+
+// Just the bare basics what's needed to validate a session.
+type SessionToken struct {
+	Token     string    `db:"token"`
+	ExpiresAt time.Time `db:"expires_at"`
 	DeletedAt time.Time `db:"deleted_at"`
 }
--- client/internal/models/users.go
+++ client/internal/models/users.go
@@ -6,3 +6,9 @@ type CreateUser struct {
 	Password     string
 	HetznerToken string
 }
+
+type GetUser struct {
+	ID       uint64
+	Email    string
+	Username string
+}
