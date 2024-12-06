450e38210fddfd9ff8285338242319311075f648
Dejan Gegic <dejangegic100@gmail.com>
2024-12-07 00:50:06 +0100

    feat: Functioning token validation

--- client/internal/auth/validate.go
+++ client/internal/auth/validate.go
@@ -6,25 +6,27 @@ import (
 	"hetz-client/internal/repository"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 func ValidateSession(c echo.Context, repo *repository.Repository) error {
-	token, _, err := getDataFromCookie(c)
+	token, userId, err := getDataFromCookie(c)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusUnauthorized)
 	}
 	fmt.Println("Token is: ", token)
+	fmt.Println("User ID is: ", userId)
+	session, err := repo.GetSessionToken(token)
+	if err != nil {
+		fmt.Println("Error getting session token: ", err)
+		return echo.NewHTTPError(http.StatusUnauthorized)
+	}
 
-	// session, err := repo.GetSessionToken(token)
-	// if err != nil {
-	// 	return echo.NewHTTPError(http.StatusUnauthorized)
-	// }
-
-	// if err := ValidateToken(session); err != nil {
-	// 	return echo.NewHTTPError(http.StatusUnauthorized)
-	// }
+	if err := ValidateToken(session, token); err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized)
+	}
 
 	return nil
 }
@@ -47,8 +49,24 @@ func getDataFromCookie(c echo.Context) (token string, userId int, err error) {
 	return cookieToken.Value, userId, nil
 }
 
-func ValidateToken(session *models.SessionToken) error {
-	fmt.Println(session.DeletedAt)
+func ValidateToken(session *models.SessionToken, cookieToken string) error {
+	if session.ExpiresAt.Before(session.ExpiresAt) {
+		fmt.Println("Session expired")
+		return echo.NewHTTPError(http.StatusUnauthorized)
+	}
+	if session.Token != cookieToken {
+		fmt.Println("Token mismatch")
+		return echo.NewHTTPError(http.StatusUnauthorized)
+	}
+
+	// take first 14 digits and convert them to time.Time
+	created_at, err := time.Parse("20060102150405", session.Token[:14])
+	if err != nil {
+		fmt.Println("Error parsing created_at: ", err)
+		return echo.NewHTTPError(http.StatusUnauthorized)
+	}
+	fmt.Println("Created at: ", created_at)
+	fmt.Println("Expires at: ", session.ExpiresAt)
 
 	return nil
 }
