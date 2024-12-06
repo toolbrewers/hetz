aeefcbdf5a1427e99f1ed85d6e8c2197e70cebee
Dejan Gegic <dejangegic100@gmail.com>
2024-12-06 17:44:10 +0100

    test(repository): simplified user test

--- client/internal/repository/users_test.go
+++ client/internal/repository/users_test.go
@@ -1,53 +1,28 @@
 package repository_test
 
 import (
-	"database/sql"
 	"testing"
 
 	"hetz-client/internal/models"
 	"hetz-client/internal/repository"
-
-	"github.com/doug-martin/goqu/v9"
 )
 
-func TestUnit_CreateUser(t *testing.T) {
-	tests := []struct {
-		name    string
-		user    *models.CreateUser
-		wantErr error
-		cleanup func(db *sql.DB)
-	}{
-		{
-			name: "happy path",
-			user: &models.CreateUser{
-				Username:     "johndoe",
-				Email:        "johndoe12@example.com",
-				Password:     "password123",
-				HetznerToken: "abc123",
-			},
-			wantErr: nil,
-			cleanup: func(db *sql.DB) {
-				query, _, _ := goqu.Delete("users").
-					Where(goqu.Ex{"username": "johndoe"}).ToSQL()
+func TestUser(t *testing.T) {
+	t.Run("CreateUser", testCreateUser)
+}
 
-				_, err := db.Exec(query)
-				if err != nil {
-					t.Errorf("unexpected error: %v", err)
-				}
-			},
-		},
+func testCreateUser(t *testing.T) {
+	repo := repository.New("../../db/client.db")
+	defer repo.Close()
+
+	user := &models.CreateUser{
+		Username:     "johndoe",
+		Email:        "johndoe12@example.com",
+		Password:     "password123",
+		HetznerToken: "abc123",
 	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			repo := repository.New("../../db/client.db")
-			defer repo.Close()
-			defer tc.cleanup(repo.DB)
-
-			_, err := repo.CreateUser(tc.user)
-			if err != tc.wantErr {
-				t.Errorf("expected: %v, got: %v", tc.wantErr, err)
-			}
-		})
+	_, err := repo.CreateUser(user)
+	if err != nil {
+		t.Errorf("expected: %v, got: %v", nil, err)
 	}
 }
