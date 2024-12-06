c2e65b9c55dd3e642a1bf98c79182f192d727c2d
Dejan Gegic <dejangegic100@gmail.com>
2024-12-06 23:33:40 +0100

    fix: removed deleted at form models as nil values cause error

--- client/internal/models/session.go
+++ client/internal/models/session.go
@@ -9,12 +9,10 @@ type Session struct {
 	ExpiresAt time.Time `db:"expires_at"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
-	DeletedAt time.Time `db:"deleted_at"`
 }
 
 // Just the bare basics what's needed to validate a session.
 type SessionToken struct {
 	Token     string    `db:"token"`
 	ExpiresAt time.Time `db:"expires_at"`
-	DeletedAt time.Time `db:"deleted_at"`
 }
