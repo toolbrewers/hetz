47c56e6c516496bdc5500ebb2bca4784f85936d9
Dejan Gegic <dejangegic100@gmail.com>
2024-12-06 17:47:04 +0100

    feat(repo): Added get user by email and delete user

--- client/internal/repository/users_test.go
+++ client/internal/repository/users_test.go
@@ -9,6 +9,8 @@ import (
 
 func TestUser(t *testing.T) {
 	t.Run("CreateUser", testCreateUser)
+	t.Run("GetUserByEmail", testGetUserByEmail)
+	t.Run("DeleteUserByID", testDeleteUser)
 }
 
 func testCreateUser(t *testing.T) {
@@ -22,6 +24,34 @@ func testCreateUser(t *testing.T) {
 		HetznerToken: "abc123",
 	}
 	_, err := repo.CreateUser(user)
+	if err != nil {
+		t.Errorf("expected: %v, got: %v", nil, err)
+	}
+}
+
+func testGetUserByEmail(t *testing.T) {
+	repo := repository.New("../../db/client.db")
+	defer repo.Close()
+
+	user, err := repo.GetUserByEmail("johndoe12@example.com")
+	if err != nil {
+		t.Errorf("expected: %v, got: %v", nil, err)
+	}
+	if user.Email != "johndoe12@example.com" {
+		t.Errorf("expected: %v, got: %v", "johndoe12@example.com", user.Email)
+	}
+}
+
+func testDeleteUser(t *testing.T) {
+	repo := repository.New("../../db/client.db")
+	defer repo.Close()
+
+	user, err := repo.GetUserByEmail("johndoe12@example.com")
+	if err != nil {
+		t.Errorf("expected: %v, got: %v", nil, err)
+	}
+
+	err = repo.DeleteUserByID(user.ID)
 	if err != nil {
 		t.Errorf("expected: %v, got: %v", nil, err)
 	}
